7ccc86864e377f33937fb615a6cbde77512966cc
Victor Zuluaga <zrvictor@outlook.com>
2024-04-14 23:26:24 -0500

    added dark mode init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-slate-900">
+    <html lang="en" className="bg-slate-900 dark">
       <body className={inter.className}>{children}</body>
     </html>
   );
