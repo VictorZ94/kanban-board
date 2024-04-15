ef6f95852a4d4fbe51b98aab5a0e82e198a7a195
Victor Zuluaga <zrvictor@outlook.com>
2024-04-14 23:27:30 -0500

    Column component

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -11,13 +11,13 @@ const Column: React.FC<Props> = ({ column, tasks }) => {
   return (
     <div className="w-full 2xl:w-96 h-min-lg bg-slate-700 rounded-xl">
       <div className="p-4 text-white font-semibold">{column.title}</div>
-      <div className="flex flex-col justify-between h-full">
+      <div className="flex flex-col justify-between min-h-72 h-full">
         <Droppable droppableId={column.id}>
           {(provided) => (
             <ul
               {...provided.droppableProps}
               ref={provided.innerRef}
-              className="px-2 space-y-2"
+              className="px-2 space-y-2 h-64"
             >
               {tasks.map((task: TaskTypes, index) => (
                 <Draggable key={task.id} draggableId={`${task.id}`} index={index}>
