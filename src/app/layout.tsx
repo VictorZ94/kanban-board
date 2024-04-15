921b0364e7897762a857546b4c508d9978de54ff
Victor Zuluaga <zrvictor@outlook.com>
2024-04-15 16:20:28 -0500

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-slate-900 dark">
+    <html lang="en" className="dark bg-slate-900">
       <body className={inter.className}>{children}</body>
     </html>
   );
