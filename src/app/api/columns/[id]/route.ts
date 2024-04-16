33261a050ec8ba41b858d8d6516f46d87529e205
Victor Zuluaga <zrvictor@outlook.com>
2024-04-16 13:31:14 -0500

    new endpoint api

---
+++ src/app/api/columns/[id]/route.ts
@@ -0,0 +1,49 @@
+import { NextResponse } from "next/server";
+import { db } from "@/libs/prisma";
+
+interface Params {
+  params: { id: string };
+}
+
+export async function GET(request: Request, { params }: Params) {
+  const columns = await db.column.findMany({
+    where: {
+      userId: Number(params.id),
+    },
+    include: {
+      tasks: {
+        select: {
+          id: true,
+        },
+      },
+    },
+  });
+
+  return NextResponse.json(columns);
+}
+
+export async function POST(request: Request, { params }: Params) {
+  try {
+    const { title } = await request.json();
+
+    const newCol = await db.column.create({
+      data: {
+        title,
+        userId: Number(params.id),
+      },
+    });
+
+    return NextResponse.json(newCol);
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        {
+          message: error.message,
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+  }
+}
