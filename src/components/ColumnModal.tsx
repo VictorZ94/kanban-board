9512c81461f56061d8e977c427a5cd9ffe62e910
Victor Zuluaga <zrvictor@outlook.com>
2024-04-16 13:32:23 -0500

    update column modal component

--- src/components/ColumnModal.tsx
+++ src/components/ColumnModal.tsx
@@ -1,22 +1,46 @@
+import { CreateColumn } from "@/utils/queries";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 import { Button, Label, Modal, TextInput } from "flowbite-react";
+import React, { FC, useState } from "react";
 
-const ColumnModal = ({ openModal, setOpenModal }) => {
+interface Props {
+  openModal: boolean
+  setOpenModal: (_: boolean) => void
+  userId: number
+}
+
+const ColumnModal: FC<Props> = ({ openModal, setOpenModal, userId }) => {
+  const [columnName, setColumnName] = useState<string>("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const columnCreated = await CreateColumn(columnName, userId);
+    console.log("columnCreated", columnCreated);
+    setOpenModal(!openModal)
+  }
+
   return (
-    <Modal show={openModal} size="md" popup onClose={() => setOpenModal(false)} className="dark:bg-slate-900">
+    <Modal show={openModal} size="md" popup onClose={() => setOpenModal(!openModal)} className="dark:bg-slate-900">
       <Modal.Header />
       <Modal.Body>
-        <div className="space-y-6">
+        <form className="space-y-6" onSubmit={handleSubmit}>
           <h3 className="text-xl font-medium text-gray-900 dark:text-white">Create column</h3>
           <div>
             <div className="mb-2 block">
               <Label htmlFor="email" value="Column name" />
             </div>
-            <TextInput id="email" placeholder="In progress" required />
+            <TextInput
+              id="email"
+              placeholder="In progress"
+              required
+              onChange={({ target }) => setColumnName(target.value)}
+            />
           </div>
           <div className="w-full">
-            <Button>Create</Button>
+            <Button type="submit">Create</Button>
           </div>
-        </div>
+        </form>
       </Modal.Body>
     </Modal>
   )
