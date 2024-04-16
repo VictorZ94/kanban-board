002363c9c5964ce69344b79548dc6805c1471c16
Victor Zuluaga <zrvictor@outlook.com>
2024-04-16 13:31:46 -0500

    navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,28 @@
 import { Button } from "flowbite-react";
 import { signOut } from "next-auth/react";
 
-function Navbar() {
+interface Props {
+  name: string | null | undefined
+}
+
+const Navbar: React.FC<Props> = ({ name }) => {
 
   return (
     <nav className="bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      {/* <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-        <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Kanban Board</span>
-      </a> */}
-      <h1 className="bg-clip-text hero-title bg-gradient-to-b from-slate-900 to-slate-900/70 dark:from-white dark:to-white/40 text-transparent border-none">
-        Kanban board app
-      </h1>
-      <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          {/* <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Logout</button>
-          <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-            <span className="sr-only">Open main menu</span>
-            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-            </svg>
-        </button> */}
-        <Button onClick={() => signOut()}>Logout</Button>
-      </div>
-      <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-        <div>
-          <span className="block py-2 px-3 rounded text-white bg-slate-900 font-bold">Welcome: </span>
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <h1 className="bg-clip-text hero-title bg-gradient-to-b from-slate-900 to-slate-900/70 dark:from-white dark:to-white/40 text-transparent border-none">
+          Kanban board app
+        </h1>
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <Button onClick={() => signOut()}>Logout</Button>
+        </div>
+        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+          <div>
+            <span className="block py-2 px-3 rounded text-white bg-slate-900 font-bold">Welcome: {name}</span>
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
   )
 }
 
