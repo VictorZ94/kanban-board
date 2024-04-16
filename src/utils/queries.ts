28cb96ac3dd9ff42bbf42a45fe9aa444ea14846c
Victor Zuluaga <zrvictor@outlook.com>
2024-04-16 17:00:44 -0500

    added new queries

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -1,4 +1,5 @@
-import { arrayToObject } from "@/utils/utils";
+import { TaskTypes } from "@/types";
+import { arrayToObject, arrayToObjectTask } from "@/utils/utils";
 import axios from "axios";
 
 export async function getColumnsByUserId(userId: number) {
@@ -40,11 +41,58 @@ interface InfoColumn {
   description: string;
 }
 
+export async function getTasks() {
+  try {
+    const tasks = await axios.get(`http://localhost:3000/api/tasks`);
+    // console.log("from quesris", tasks);
+    if (tasks.status === 200 && tasks.data.length > 0) {
+      const tasksData = arrayToObjectTask(tasks.data);
+      // console.log("tasksData", tasksData);
+      return tasksData;
+    } else {
+      return {};
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 export async function CreateTask(columnId: number, data: InfoColumn) {
   try {
     const newColumn = await axios.post(
-      `http://localhost:3000/api/task/${columnId}`,
+      `http://localhost:3000/api/tasks/${columnId}`,
       data
+    );
+    if (newColumn.status === 200) {
+      return newColumn.data;
+    } else {
+      return {};
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function UpdateTask(data: TaskTypes) {
+  try {
+    const taskUpdated = await axios.put(
+      `http://localhost:3000/api/tasks/${data.id}`,
+      data
+    );
+    if (taskUpdated.status === 200) {
+      return taskUpdated.data;
+    } else {
+      return {};
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function RemoveTask(taskId: number) {
+  try {
+    const newColumn = await axios.delete(
+      `http://localhost:3000/api/tasks/${taskId}`
     );
     if (newColumn.status === 200) {
       return newColumn.data;
